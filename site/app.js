4a150c70b7ee48b67cdeb471c554ea9b160aaefd
Hugh Wang <hghwng@gmail.com>
2015-05-16 20:22:52 +0800

    Add query support

--- site/app.js
+++ site/app.js
@@ -8,20 +8,37 @@ fs.readFile('data.json', 'utf8', function(err, str) {
 	g_data = JSON.parse(str);
 });
 
+function show_word(key, resp)
+{
+	var info = g_data[key];
+	resp.writeHead(200, { 'Content-Type': 'application/json' });
+	if (info == undefined) {
+        resp.write('{}');
+    } else {
+        resp.write(JSON.stringify(info));
+    }
+	resp.end();
+}
+
 var server = http.createServer(function(req, resp) {
 	req_url = url.parse(req.url, true);
 
 	if (req_url.query['action'] == 'quiz') {
+
 		var keys = Object.keys(g_data);
 		var key = keys[Math.floor(Math.random() * keys.length)];
-		var info = g_data[key];
-		resp.writeHead(200, { 'Content-Type': 'application/json' });
-		resp.write(JSON.stringify(info));
-		resp.end();
+		show_word(key, resp);
+
+	} else if (req_url.query['action'] == 'query') {
+
+		var key = req_url.query['word'];
+		show_word(key, resp);
+
 	} else {
 
 		resp.writeHead(200, { 'Content-Type': 'text/html' });
 		fs.createReadStream('./assets/index.html').pipe(resp);
+
 	};
 });
 server.listen(8080);
